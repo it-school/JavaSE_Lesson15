4298ed6b48ae0dfc26c3f68d3a3aa863edbbabc3
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2018-11-18 23:50:20 +0200

    Update Employee.java

--- src/com/itschool/Employee.java
+++ src/com/itschool/Employee.java
@@ -3,7 +3,7 @@ package com.itschool;
 public class Employee implements java.io.Serializable{
     public String name;
     public String address;
-    public transient int number;  // поля с ключевім словом transient НЕ сериализуются
+    public transient int number;  // поля с ключевым словом transient НЕ сериализуются
 
     public void mailCheck() {
         System.out.println("Mailing a check to " + name + " " + address);
